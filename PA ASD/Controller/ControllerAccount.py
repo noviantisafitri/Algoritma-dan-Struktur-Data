26fa1c7848989ff1a3ed6a959f884081a0207bef
noviantisafitri <121856489+noviantisafitri@users.noreply.github.com>
2023-04-03 08:37:07 +0800

    Update ControllerAccount.py

--- PA ASD/Controller/ControllerAccount.py
+++ PA ASD/Controller/ControllerAccount.py
@@ -11,14 +11,14 @@ class Account(Database): #akun
         self.akun_nim = []
         super().__init__()
 
-    def cekNim(self, nim):
+    def cekNim(self, nim): #melakukan pengecekan nim di database
         if self.find_nim(nim):
-            # print("NIM TELAH TERDAFTAR")
             return True
         else :
             return False
+        
     def registrasi(self, nim,nama, prodi, jeniskelamin, pasw):
-        if nim != self.find_nim(nim) :
+        if nim != self.find_nim(nim) : #menambahkan data 
             self.nim.append(nim)
             self.nama.append(nama)
             self.prodi.append(prodi)
@@ -32,28 +32,27 @@ class Account(Database): #akun
                     "jk" : jeniskelamin,
                     "password" : password}
             self.akun_nim.append(akun)
-        self.insert_akun(self.akun_nim)
+        self.insert_akun(self.akun_nim) #menambahkan data ke dalam database
 
-    def login(self, cari, password):
+    def login(self, cari, password): #melakukan pengecekan akun untuk login
         if cari:
-            if password != cari.get("password"):
+            if password != cari.get("password"): #jika username dan password akun tidak sesuai
                 print("Password salah")
                 return False
             else :
-                return True
+                return True #jika username ditemukan dan password sesuai
         else:
-            print("Akun tidak ditemukan")
+            print("Akun tidak ditemukan") #jika username tidak ditemukan
             return False
                  
-    def profil_mhs(self, nim):
-        # print(self.find_nim(nim))
+    def profil_mhs(self, nim): #menampilkan data profil mahasiswa
         profil = PrettyTable(["NIM", "Nama", "Program Studi", "Jenis Kelamin"])
         profil.title = "Profil Mahasiswa"
         profil.add_row([self.find_nim(nim).get("nim"), self.find_nim(nim).get("nama"), self.find_nim(nim).get("prodi"), self.find_nim(nim).get("jk")])
         print(profil)
 
-    def profil_adm(self,nip):
-        profil = PrettyTable(["NIP", "Nama", "Tanggal Lahir", "Jenis Kelamin",])
-        profil.title = "Profil Mahasiswa"
-        profil.add_row([self.find_nip(nip).get("nip"), self.find_nip(nip).get("nama"),self.find_nip(nip).get("prodi"), self.find_nip(nip).get("jk")])
+    def profil_adm(self, nip): #menampilkan data profil staff
+        profil = PrettyTable(["NIP", "Nama", "Jenis Kelamin", "Jabatan",])
+        profil.title = "Profil Staff"
+        profil.add_row([self.find_nip(nip).get("nip"), self.find_nip(nip).get("nama"),self.find_nip(nip).get("jk"), self.find_nip(nip).get("jabatan")])
         print(profil)

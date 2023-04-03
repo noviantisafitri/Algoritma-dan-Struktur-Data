38ef91773ef73d7b0e4df4f64e16bf75e3e964c6
noviantisafitri <121856489+noviantisafitri@users.noreply.github.com>
2023-04-03 08:38:49 +0800

    Update Database.py

--- PA ASD/Model/Database.py
+++ PA ASD/Model/Database.py
@@ -10,18 +10,18 @@ class Database:
         self.akun_collection = akun.akun_collection
         self.data_collection = data.data_collection
     
-    def find_nim (self, nim):
+    def find_nim (self, nim): #mencari nim didatabase sesuai dengan nim yang diinputkan
         nim_f = self.akun_collection.find_one({"nim" : nim})
         return nim_f
     
-    def find_nip (self, nip):
+    def find_nip (self, nip): #mencari nip didatabase sesuai dengan nip yang diinputkan
         nip_f = self.akun_collection.find_one({"nip" : nip})
         return nip_f
     
-    def insert_akun (self, data):
+    def insert_akun (self, data): #menambahkan data registrasi akun mahasiswa ke database
         self.akun_collection.insert_many(data)
 
-    def id_peminjaman(self):
+    def id_peminjaman(self): #membuat id peminjaman
         nomor = self.data_collection.find()
         no = 1100
         if nomor is not None:
@@ -30,35 +30,34 @@ class Database:
         no += 1
         return no
 
-    def insert_data(self, data):
+    def insert_data(self, data): #melakukan insert data ke database
         self.data_collection.insert_one(data).inserted_id
     
     def insert_to_database(self, no, nim, kode, nama, prodi, mk, keperluan, tanggal_p, tanggal_selesai, status):
-        data = { "no" : no,
+        data = { "no" : no,  #memasukan data peminjaman oleh user dan staff ke database
         "kode" : kode,
         "nim" : nim,
         "nama" : nama,
         "prodi" : prodi,
         "mk" : mk,
         "keperluan" : keperluan,
-        "tanggal p" : tanggal_p,
-        "tanggal s" : tanggal_selesai,
+        "tanggalp" : tanggal_p,
+        "tanggals" : tanggal_selesai,
         "status" : status,
         "ket" : "Digunakan"}
         self.insert_data(data)
 
-    def find_data (self, cari):
+    def find_data (self, cari): #mencari satu data kode kelas didatabase sesuai dengan inputan
         cek = self.data_collection.find_one({"kode" : cari})
         return cek
-    def find_all (self):
+    
+    def find_kelas (self, cari): #mencari seluruh data kode kelas didatabase sesuai dengan inputan
+        cek = self.data_collection.find({"kode" : cari})
+        return cek
+    
+    def find_all (self): #mencari semua data didatabase
         all = self.data_collection.find()
         return all
-    def find_all_null (self):
-        data = self.data_collection.find()
-        return data
-    
-        # for i in data:
-        #     data = [i["kode"],i["nim"],i["nama"],i["prodi"],i["mk"],i["keperluan"],i["tanggal p"],i["tanggal s"],i["status"],i["ket"]]
 
     def update_data (self, no, status): #update data di database
         return self.data_collection.update_one(no, status)
@@ -66,9 +65,6 @@ class Database:
     def delete_data (self, id): #delete data di database
         self.data_collection.delete_one({"no" : id })
 
-    def delete_data_tanggal(self, tanggal):
-        self.data_collection.delete_one({"tanggal s" : tanggal})
-
-    def finduser_bp(self):
-        return self.data_collection.find_one({"no" : 1101})
+    def finduser_bp(self, id): #mencari data didatabase berdasarkan no id
+        return self.data_collection.find_one({"no" : id})
         

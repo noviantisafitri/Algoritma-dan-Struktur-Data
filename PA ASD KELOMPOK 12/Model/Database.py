0c53424c554397e0e49f4cc9351d7b5501e97439
noviantisafitri <121856489+noviantisafitri@users.noreply.github.com>
2023-04-11 21:03:17 +0800

    Update Database.py

--- PA ASD KELOMPOK 12/Model/Database.py
+++ PA ASD KELOMPOK 12/Model/Database.py
@@ -4,8 +4,8 @@ from pymongo import MongoClient
 class Database:
     def __init__(self):
 
-        client = MongoClient("mongodb+srv://noviantis112:21november@cluster1.nsfjyfw.mongodb.net/?retryWrites=true&w=majority")
-        database = client["PA-ASD-KELOMPOK12"]
+        client = MongoClient("mongodb://novianti:Ah7iRtN5AFbSnkYMLhDEgjAFeR5XxjQMCidRSQhE@sandbox-shard-00-00.u1rbk.mongodb.net:27017/?ssl=true")
+        database = client["room_reserve"]
         
         self.akun_collection = database["akun"]
         self.data_collection = database["data"]
